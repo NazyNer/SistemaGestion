801626295c458300a75e39ee2dddc1313f02f95a
Arcangel <nemaldonado663@gmail.com>
2025-06-10 18:11:37 -0300

    Roles Finiquitadisimo

--- Services/RoleInitializerService.cs
+++ Services/RoleInitializerService.cs
@@ -8,6 +8,7 @@ using System.Runtime.CompilerServices;
 public interface IRoleInitializerService
 {
     Task<JsonResult> InicializarRolesAsync();
+    
 }
 public class RoleInitializerService : IRoleInitializerService
 {
@@ -24,15 +25,24 @@ public class RoleInitializerService : IRoleInitializerService
         Console.WriteLine("Ingresando roles...");
         var Roles_necesarios = new[] { "Administrador", "Cliente", "Proveedor", "Vendedor" };
 
-        Console.WriteLine("Verificando roles...");
+        Console.WriteLine("Verificando roles existentes en base de datos...");
         var Roles_existentes = await _context.Roles.Where(r => Roles_necesarios.Contains(r.Name)).Select(r => r.Name).ToListAsync();
 
         Console.WriteLine("Verificando roles...");
         var Roles_faltantes = Roles_necesarios.Except(Roles_existentes).ToList();
-        foreach (var rol in Roles_faltantes)
+        if (Roles_faltantes.Count > 0)
         {
-            await _roleManager.CreateAsync(new IdentityRole(rol));
+            foreach (var rol in Roles_faltantes)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(rol));
+            }
+            
         }
+        else
+        {
+            Console.WriteLine("No hay roles para crear");
+        }
+        
         Console.WriteLine("Roles finalizado👍");
         return new JsonResult(Roles_necesarios);
     }
